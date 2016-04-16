0b974be834efe23175f93dae616a60560a1b3e31
JasonEtco <jasonetco@gmail.com>
2016-04-15 21:05:37 -0400

    Converted tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,42 +10,42 @@
 
 module.exports = function(grunt) {
 
-	// Project configuration.
-	grunt.initConfig({
-		jshint: {
-			all: [
-				'Gruntfile.js',
-				'tasks/*.js'
-			],
-				options: {
-				jshintrc: '.jshintrc'
-			}
-		},
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js'
+      ],
+        options: {
+        jshintrc: '.jshintrc'
+      }
+    },
 
-		jekyll_post: {
-			options: {
-				questions: [
-					{
-						config: 'title',
-						message: 'Title?',
-						default: 'Your Default Title'
-					},
-					{
-						config: 'author',
-						type: 'list',
-						message: 'Author?',
-						default: 'Your Default Author',
-						choices: ['Jason', 'Daniel']
-					}
-				]
-			}
-		}
-	});
+    jekyll_post: {
+      options: {
+        questions: [
+          {
+            config: 'title',
+            message: 'Title?',
+            default: 'Your Default Title'
+          },
+          {
+            config: 'author',
+            type: 'list',
+            message: 'Author?',
+            default: 'Your Default Author',
+            choices: ['Jason', 'Daniel']
+          }
+        ]
+      }
+    }
+  });
 
-	grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks');
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	grunt.registerTask('default', ['jshint', 'jekyll_post']);
+  grunt.registerTask('default', ['jshint', 'jekyll_post']);
 
 };
